caca608c5766f967cb44f2dc0deda609de8b783d
Пашкво Евгений Александрович <pashkoveval@gmail.com>
2025-07-06 13:05:13 +0300

    db

--- src/main.ts
+++ src/main.ts
@@ -6,20 +6,6 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
-// import { worker } from '../mock/browser';
-
-if (import.meta.env.DEV) {
-  const { worker } = await import('../mock/browser');
-  worker
-    .start({
-      serviceWorker: {
-        url: '/mockServiceWorker.js', // Явно указываем путь
-      },
-      onUnhandledRequest: 'bypass', // Игнорировать необработанные запросы
-    })
-    .catch(console.error);
-}
-
 const app = createApp(App);
 
 app.use(createPinia());
